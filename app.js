eb645532843aa0257d194dbe51a31ed134d213da
Antoine Sauray <antoine@goodway.io>
2016-05-12 00:57:07 +0200

    initial commitwq

--- app.js
+++ app.js
@@ -1,30 +1,23 @@
 var express = require('express');
-var bodyParser  = require('body-parser');
-var models = require('./libs/models')
-
 var app = express();
+var bodyParser  = require('body-parser');
+var morgan      = require('morgan');
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(morgan('dev'));
 
-// models singleton
-//app.set('models', models);
+var routes = require('./lib/routes/index');
+var message  = require('./lib/routes/message');
 
-// ----- Initialise Server
-models.sequelize.sync().then(function(){
+app.use('/api/v1', routes);
+app.use('/api/v1/message', message);
 
-  // Si database OK on lance le serveur.
-  var server = app.listen(8080, function () {
-    var host = server.address().address
-    var port = server.address().port
-    console.log("Hub infotel API listening at http://%s:%s", host, port)
-  });
 
-}).catch(function(error) {
-  "Database connection issue: " + error;
-});
+var server = app.listen(8088, function () {
 
-// ----- Routes
-app.get('/', function(req, res) {
-    res.setHeader('Content-Type', 'application/json');
-    res.json({ success: true, message: 'API on !'});
-});
+  var host = server.address().address
+  var port = server.address().port
+  console.log("Infotel API listening at http://%s:%s", host, port)
+
+})
